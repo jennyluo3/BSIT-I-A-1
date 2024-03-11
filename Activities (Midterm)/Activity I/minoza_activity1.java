033de2d9f2ef2a2deef438d52bb5bdf034a85dcc
MAYLENE M. MINOZA <156988633+Maylene99@users.noreply.github.com>
2024-03-11 10:31:10 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/minoza_activity1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MINOZA_ACTIVITY_1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String[] names = new String[5];
+
+        for(int i=0; i < 5; i++){
+            System.out.println("Enter name" +(i+1)+":");
+            names[i]= sc.nextLine();
+        }     
+        int choice;
+        System.out.println("Enter the number on the list of names you want to display: ");
+        choice = sc.nextInt();
+        
+        if(choice >= 1 && choice <= 5){
+            System.out.println("You have selected" + names[choice]);
+        }
+        else {
+            System.out.println("Invalid input.");
+        }
+    }
+}
