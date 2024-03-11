0d9c4be4701450c321c03c439bc3d28e8c5dc997
Irish Camille Y. Ipili <157033544+irishcamilleipili@users.noreply.github.com>
2024-03-11 10:27:46 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/IPILI_IRISH_ACTIVITY_I.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class IPILI_IRISH_ACTIVITY_I{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        
+        String[]names = new String[5];
+        
+        for(int i = 0; i < 5; i++) {
+            System.out.print("Enter name" + (i+1) + ":");
+            names[i] = sc.nextLine();
+        }
+
+        int choice;
+        System.out.println("Enter the number on the list of names you want to display: ");
+        choice = sc.nextInt();
+
+        if(choice >= 1 && choice <= 5){
+            System.out.println("you have selected" + names[choice - 1]);
+        }
+        else{
+            System.out.println("Invalid");
+        }
+
+
+    }
+}
