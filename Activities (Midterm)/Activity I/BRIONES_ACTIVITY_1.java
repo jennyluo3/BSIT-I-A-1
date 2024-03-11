64ed7dc5b243a9b13a0d4c7201636e59f45c3e77
Gessyl Mae F. Briones <156914040+gessylbriones@users.noreply.github.com>
2024-03-11 10:29:44 +0800

    Update BRIONES_ACTIVITY_1.java

--- Activities (Midterm)/Activity I/BRIONES_ACTIVITY_1.java
+++ Activities (Midterm)/Activity I/BRIONES_ACTIVITY_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class BRIONES_ACTIVITY_1{
-    public static void main(Sring []args){
+    public static void main(String []args){
      Scanner sc = new Scanner(System.in);
 
      String[] names = new String[5];
