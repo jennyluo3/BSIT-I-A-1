24d12e019cb05fc31b8f0ad1f7a67e912f4b1707
khentalferez <158979596+khentalferez@users.noreply.github.com>
2024-03-11 10:32:08 +0800

    Update ALFORNON_ACTIVITY_1.java

--- Activities (Midterm)/Activity I/ALFORNON_ACTIVITY_1.java
+++ Activities (Midterm)/Activity I/ALFORNON_ACTIVITY_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SARCON_ACTIVITY_I {
+public class ALFORNON_ACTIVITY_I {
      
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in); 
