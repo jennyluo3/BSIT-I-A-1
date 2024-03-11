b9482fd4c666cec61b0f2f7406a04b12af01d931
Eldrie Yancy C. Icoy <158005676+EldrieYancyIcoy@users.noreply.github.com>
2024-03-11 10:28:45 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/ICOY_ACTIVITY_I.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ICOY_ACTIVITY_I{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String[] names = new String[5];
+
+        for (int i = 0; i < names.length; i++){
+            System.out.print("Enter name" + (i + 1) + ":");
+            names[i] = sc.nextLine();
+        }
+
+        int choice;
+        System.out.print("Enter the number on the list of names you want to display: ");
+        choice = sc.nextInt();
+
+        if (choice >= 1 && choice <= 5) {
+            System.out.print("You have selected" + names[choice - 1]);
+        }
+        else {
+            System.out.print("Invalid input");
+        }
+    }
+}
