e7416d88edb3c10360b686a447e7ada515049fad
kim louie N. olid <158005486+kimkim2003@users.noreply.github.com>
2024-03-11 11:06:33 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/[OLID_ACTIVITY_I].java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class [OLID_ACTIVTY_I] {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        String[] names = new String [5];
+        System.out.print("Enter a number: ");
+
+        for (int i = 0 ; _ 5;++;){
+            System.out.print("Name" + (i + 1) + ":");
+            Name [1] = Scanner.nextline();
+        } 
+        System.out.print(s:"Enter a number between 1 and 5 to select a name: ");
+        int index = sc.nextlnt();
+
+        if (index < 1 || index > 5){
+                System.out.print(s:"Invalid input. Number must be between 1 and 5");
+                return;     
+        }
+        System.out.println("Selected name:" + names[index - 1] );
+
+
+    }     
+
+}    
+        
+    
+    
+
+
+
