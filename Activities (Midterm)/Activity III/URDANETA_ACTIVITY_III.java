ca399ab5207067e0d73598a574cd67dc658bb8ca
Archie Urdaneta <158005780+ArchieUrdaneta@users.noreply.github.com>
2024-03-18 10:33:33 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/URDANETA_ACTIVITY_III.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class URDANETA_ACTIVITY_III{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [][] score = new int [5][5];
+
+        for (int x = 0; x < score. length; x++){
+            for (int y = 0; y < score.length; y++){
+                System.out.print("Enter score for row" + (x + 1) +", column" + (y+1) + ":");
+                score[x][y] = sc.nextInt();
+            }
+        }
+
+        int sum = 0;
+        int count = 0;
+        for (int x = 0; x < score. length; x++) {
+            for (int y = 0; y < score.length; y++) {
+                 sum += score [x][y]; 
+                 count++;
+            }
+        }
+ 
+        double average = (double) sum / count;
+        System.out.println("The Average of the Scored: " + average);
+    }
+}
