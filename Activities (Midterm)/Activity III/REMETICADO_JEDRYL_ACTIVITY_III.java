87fcb25a5397c4141665f5ee6832e05231b0d8f7
Jedryl Remeticado <158005371+jeduuujed@users.noreply.github.com>
2024-03-18 10:35:01 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/REMETICADO_JEDRYL_ACTIVITY_III.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class REMETICADO_JEDRYL_ACTIVITY_III {
+    public static void main(String []args) {
+
+        double[][] scores = new double[5][5];
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter scores:");
+
+        for (int i = 0; i < 5; ++i) {
+            for (int j = 0; j < 5; ++j) {
+                scores[i][j] = scanner.nextDouble();
+            }
+        }
+
+        double sum = 0;
+        for (int i = 0; i < 5; ++i) {
+            for (int j = 0; j < 5; ++j) {
+                sum += scores[i][j];
+            }
+        }
+
+        double average = sum / 25.0;
+
+        System.out.println("Average of all scores: " +average);
+
+        scanner.close();
+
+
+    }
+}
