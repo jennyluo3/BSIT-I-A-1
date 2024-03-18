d7b4e7dfd6959c6757e7a9cdb293e2439eeb6b32
Jaild Katte C. Lapis <158005981+jaildkatte@users.noreply.github.com>
2024-03-18 10:31:25 +0800

    ALFEREZ_ACTIVITY_III.java

--- Activities (Midterm)/Activity III/ALFEREZ_ACTIVITY_III.java
+++ Activities (Midterm)/Activity III/ALFEREZ_ACTIVITY_III.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+public class ALFEREZ_ACTIVITY_III {
+       public static void main(String [] args) {
+            
+        int[][] score = new int[5][5];
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter 5 score separated by space or new line: ");
+
+        for (int i = 0; i < 5; ++i){
+            for (int j = 0; j<5; ++j){
+                score[i][j] = scanner.nextInt();
+
+            }
+        }
+        scanner.close();
+        int sum = 0;
+        for (int i = 0; i<5; ++i){
+            for (int j = 0; j<5; ++j){
+                sum += score[i][j];
+            }
+        }
+        double average = sum / 25.0;
+        
+        System.out.println("Average score: " + average);
+        
+        
+        
+    }
+}
