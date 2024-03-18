540b88d47f0dd1b4efa8db0e1f1cea7d7a680ced
Johan Carl Demotor <157077716+johancarldemotor@users.noreply.github.com>
2024-03-18 10:39:59 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/DEMOTOR_ACTIVITY_lll.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+    public class DEMOTOR_ACTIVITY_III {
+        public class void main(string[]args) {
+            Scanner sc= new Scanner(System.in);
+
+            int[][]scores = new int[5][5];
+
+            for(int x=0; x<score.length; x++){
+            for(int y=0; y<score.length; y++){
+                System.out.println("Please Enter Score:" + (x + 1) + "-" + (y + 1) + ":");
+                score[x][y] = sc.nextInt();       
+        }
+    }  
+
+    int sum = 0
+    int count = 0
+    for(int x = 0; x<score.length; x++) {
+    for(int y = 0; y<score.length; y++) {
+        sum += score [x][y];
+        count++;
+    }
+    }
+
+    double average = (double) sum / count;
+    System.out.println("The Average of Score that you entered: " average);
+}
+}
