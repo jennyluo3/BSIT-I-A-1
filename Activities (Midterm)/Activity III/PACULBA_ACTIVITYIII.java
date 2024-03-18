c9ab8478a22756c31162e93b1f80f21e634792ea
Paculba,Angelyn <158006374+paculbaange@users.noreply.github.com>
2024-03-18 10:31:46 +0800

    PACULBA_ACTIVITY III.java

--- Activities (Midterm)/Activity III/PACULBA_ACTIVITYIII.java
+++ Activities (Midterm)/Activity III/PACULBA_ACTIVITYIII.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PSCULBA ACTIVITY_III{
+public class PACULBA ACTIVITY_III{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         int [][] score = new int [5][5];
