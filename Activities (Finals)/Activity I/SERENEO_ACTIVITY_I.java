992e1b774656027b1b2a5bbcf5d661f387998195
Sereneo Jiethmar C <158004974+sjiethmar@users.noreply.github.com>
2024-04-08 10:52:48 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/SERENEO_ACTIVITY_I.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class SERENEO_ACTIVITY_I {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int lenght = 20, width = 40;
+        System.out.println("Average:"+lenght + width/2);
+        System.out.println("Area:"+lenght + width);
+    }
+}
