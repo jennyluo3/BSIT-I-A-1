205baffc112ee30f19fb6a683c5431c12108b2e2
Van Cyril Ballori <158006267+VanCyrilBallori@users.noreply.github.com>
2024-04-08 10:51:10 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/BALLORI_ACTIVITY_I.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class BALLORI_ACTIVITY_I{
+public static void main(String[] args) {
+    int length = getLength();
+    int width = getWidth();
+    int area = length * width;
+    System.out.println("Area =" +area);
+}
+public static int getLength(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the length of the Rectangle: ");
+    return sc.nextInt();
+}
+public static int getWidth(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the width of the Rectangle: ");
+    return sc.nextInt();
+}
+}
