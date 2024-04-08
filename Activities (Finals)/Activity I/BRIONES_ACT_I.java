f421c2828b6525f01074553fd36fd64962e4d174
Gessyl Mae F. Briones <156914040+gessylbriones@users.noreply.github.com>
2024-04-08 10:49:26 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/BRIONES_ACT_I.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BRIONES_ACT_I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = 20, width = 40;
+        System.out.println("Average: " + length + width / 2);
+        System.out.println("Area: " + length * width);
+
+    }
+}
