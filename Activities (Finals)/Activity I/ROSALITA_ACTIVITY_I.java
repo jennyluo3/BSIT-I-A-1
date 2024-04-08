a7013bef89fff924d6eab0513119274643e5b33b
A Jay R. Rosalita <157581445+ajayrosalita5@users.noreply.github.com>
2024-04-08 10:59:11 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/ROSALITA_ACTIVITY_I.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class ROSALITA_ACTIVITY_I {
+   public static void main(String []args){
+    int length = getLength();
+    int width = getWidth();
+    int area = length * width;
+    System.out.println("Area = "+area);
+   }
+
+   public static int getLength(){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the length of the rectangle: ");
+    return sc.nextInt();
+   }
+
+   public static int getWidth(){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the width of the rectangle: ");
+    return sc.nextInt();
+   }
+    }
