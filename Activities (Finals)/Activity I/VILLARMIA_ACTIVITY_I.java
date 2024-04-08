ca2b6a0ef54715acc45c4a07693ba17d4bf3a58f
Gian Gadiano Villarmia <157211284+GianVillarmia18@users.noreply.github.com>
2024-04-08 10:54:49 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/VILLARMIA_ACTIVITY_I.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class VILLARMIA_ACTIVITY_I{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int length = 20, width = 40;
+        System.out.println("Average: " + length + width / 2);
+        System.out.println("Area: " + length * width);
+    }
+}
