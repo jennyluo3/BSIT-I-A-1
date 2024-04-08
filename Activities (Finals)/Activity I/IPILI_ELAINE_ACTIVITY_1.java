65c9673f783b22a2bd75ed89741cfb6fee81167c
Elaine Nicole Ipili <157080275+ipilielainenicole@users.noreply.github.com>
2024-04-08 10:51:57 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/IPILI_ELAINE_ACTIVITY_1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class IPILI_ELAINE_ACTIVITY_1 {
+ static int lenght(){scanner,output,return sc.nextInt();
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Enter the length of the rectangle:");
+    return sc.nextInt();
+}
+static int width(){
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Enter the width of the rectangle:");
+    return sc.nextInt();
+}
+public static void main(String[]args);
+
+
+ int result=
+ System.out.println("Area of the rectangle;" +lenght()*width());
+}
+
