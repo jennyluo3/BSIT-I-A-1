62021fcb738d5bd891a7da71a1a00a47ac439ebf
Lovenel Canillo <158016531+Lovenel19@users.noreply.github.com>
2024-04-08 10:54:29 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/CANILLO_ACTIVITY_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class CANILLO_ACTIVITY_1{
+    
+        static int getLength(){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle:");
+        return sc.nextInt();
+        }
+        static int getWidth(){
+            Scanner sc= new Scanner(System.in);
+            System.out.print("Enter the width of the rectangle:");
+            return sc.nextInt();
+        }
+
+        public static void main(String[] args){
+            int lenght = getLength();
+            int width = getWidth();
+
+            int area = lenght*width;
+
+            System.out.println("Area:" +area);
+        }
+}
