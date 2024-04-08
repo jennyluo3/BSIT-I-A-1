d7ab746e0eb8e79606fcb1546fb4ce2fd6e9c6bf
Irish Camille Y. Ipili <157033544+irishcamilleipili@users.noreply.github.com>
2024-04-08 11:04:48 +0800

    IPILI_IRISH_ACTIVITY_I.java

--- Activities (Finals)/Activity I/IPILI_IRISH_ACTIVITY_I.java
+++ Activities (Finals)/Activity I/IPILI_IRISH_ACTIVITY_I.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class IPILI_IRISH_ACTIVITY_I{
-    public static void main(String{}args){
+    public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
     
         int length= 20,width=10;
