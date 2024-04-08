53fc08446cd559d68ed53dae032acf10b4f6cbf3
Paculba,Angelyn <158006374+paculbaange@users.noreply.github.com>
2024-04-08 10:53:30 +0800

    PACULBA_ACTIVITY_l.java

---
+++ Activities (Finals)/Activity I/PACULBA_ACTIVITY_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class PACULBA_ACTIVTY_I{
+    static int length(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle: ");
+        return sc.nextInt();
+    }
+    static int width(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the width of the rectangle: ");
+        return sc.nextInt();
+    }
+    public static void main(String [] args){
+        long area= (long)length() * (long)width();
+
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
