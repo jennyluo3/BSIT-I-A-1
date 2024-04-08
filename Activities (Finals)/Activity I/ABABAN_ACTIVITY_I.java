46c86003398a17fe9b73248f65208cf22d7e677a
Kyle Marion N. Ababan <158006248+kyleababan@users.noreply.github.com>
2024-04-08 10:51:15 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/ABABAN_ACTIVITY_I.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ABABAN_ACTIVITY_I {
+   public static void main(String []args){
+    int length = getLength();
+    int width = getWidth();
+    int area = length * width;
+    System.out.println("Area = "+area);
+   }
+
+   public static int getLength(){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the length of the rectangle: ");
+    return sc.nextInt();
+   }
+
+   public static int getWidth(){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the width of the rectangle: ");
+    return sc.nextInt();
+   }
+    }
+
+
