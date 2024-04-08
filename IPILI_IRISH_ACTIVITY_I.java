580d2ed635409f08c6d58fcbd0550c6237d2c51e
Irish Camille Y. Ipili <157033544+irishcamilleipili@users.noreply.github.com>
2024-04-08 10:53:06 +0800

    IPILI_IRISH_ACTIVITY_I.java

--- IPILI_IRISH_ACTIVITY_I.java
+++ IPILI_IRISH_ACTIVITY_I.java
@@ -1,11 +1,11 @@
+import java.util.Scanner;
 public class IPILI_IRISH_ACTIVITY_I{
-    public static void main(String[]args){
-        int a = 12 , b = 3;
-        System.out.println("SUM= "+(a+b));
-        System.out.println("DIFFERENCE= "+(a-b));
-        System.out.println("PRODUCT =" +(a*b));
-        System.out.println("QUOTIENT =" +(a/b));
-
-
+    public static void main(String{}args){
+        Scanner sc = new Scanner(System.in);
+    
+        int length= 20,width=10;
+        System.out.println("Average:"+ length + width / 2);
+        System.out.println("Area:"+ length * width);
+        
     }
 }
