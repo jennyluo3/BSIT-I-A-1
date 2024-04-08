311af7c0a7de8883b59f9d8d97bbb98c89059b1b
Jedryl Remeticado <158005371+jeduuujed@users.noreply.github.com>
2024-04-08 10:50:12 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/REMETICADO_ACTIVITY_I.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class REMETICADO_ACTIVITY_I {
+    public static void main(String[] args) {
+       
+        double length = getLength();
+        double width = getWidth();
+
+       
+        double area = calculateArea(length, width);
+
+       
+        System.out.println("Area of the rectangle: " + area);
+    }
+
+ 
+    public static double getLength() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter length of the rectangle: ");
+        return scanner.nextDouble();
+    }
+
+    
+    public static double getWidth() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter width of the rectangle: ");
+        return scanner.nextDouble();
+    }
+
+    
+    public static double calculateArea(double length, double width) {
+        return length * width;
+    }
+}
