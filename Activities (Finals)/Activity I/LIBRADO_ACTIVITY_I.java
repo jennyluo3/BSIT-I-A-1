75ee411808a1d119fd654e3e8558a38be5be8412
John kenneth Librado <156989949+JohnKenneth16@users.noreply.github.com>
2024-04-08 10:51:21 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/LIBRADO_ACTIVITY_I.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class LIBRADO_ACTIVITY_I{
+    Scanner sc = new Scanner(System.in);
+public static void main(String [] args){
+    int length = 20, width = 40;
+    System.out.println("Average: " + length + width / 2);
+    System.out.println("Area: " + length * width);
+
+  }
+ }
