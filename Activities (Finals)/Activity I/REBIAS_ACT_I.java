e7d4661f040c3ca4af6685dfb25b7becaf01aabf
JACK NEL REBIAS <156990141+JackNel395@users.noreply.github.com>
2024-04-08 10:50:28 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/REBIAS_ACT_I.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class REBIAS_ACT_I {
+    public static void main(String[] args) {
+        int length = 20, width = 40;
+        System.out.println("Average: " + length + width / 2);
+        System.out.println("Area: " + length * width);
+
+    }
+}
