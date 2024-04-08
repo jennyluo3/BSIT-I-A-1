405d72fdf982fe4a546f48102b0c8fb9bed4453e
Irish Camille Y. Ipili <157033544+irishcamilleipili@users.noreply.github.com>
2024-04-08 10:54:36 +0800

    IPILI_IRISH_ACTIVITY_I.java

--- Activities (Finals)/Activity I/IPILI_IRISH_ACTIVITY_I.java
+++ Activities (Finals)/Activity I/IPILI_IRISH_ACTIVITY_I.java
@@ -3,7 +3,7 @@ public class IPILI_IRISH_ACTIVITY_I{
     public static void main(String{}args){
         Scanner sc = new Scanner(System.in);
     
-        int length= 20,width=40;
+        int length= 20,width=10;
         System.out.println("Average:"+ length + width / 2);
         System.out.println("Area:"+ length * width);
         
