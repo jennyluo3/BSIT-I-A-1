78bced493bdae81afc08eaa502d6c2c57a8741be
Irish Camille Y. Ipili <157033544+irishcamilleipili@users.noreply.github.com>
2024-04-15 10:46:45 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/IPILI_IRISH_ACTIVITY_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class IPILI_IRISH_ACTIVITY_II{
+     static void feetToInches(double feet){
+        double res = feet*12;
+        System.out.println(res);
+     }
+        public static void main(String[]args){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the feet:");
+            double feet = sc.nextDouble();
+            feetToInch(feet);
+        }
+
+       
+    }
