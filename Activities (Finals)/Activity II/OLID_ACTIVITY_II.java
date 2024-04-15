dfd81b98d6380560ecf141c9fa532e167638859b
kim louie N. olid <158005486+kimkim2003@users.noreply.github.com>
2024-04-15 10:52:39 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/OLID_ACTIVITY_II.java
@@ -0,0 +1,13 @@
+import java.util.scanner;
+public class OLID_ACTIVITY_II {
+    static void feetToInches(double feet) {
+        double res = feet * 12;
+        System.out.print(res);
+    }
+    public static void main(String[]args){
+        int feet = getfeet();
+        int inches = feet * 12;
+        System.out.print(inches);
+    }
+}
+ }   
