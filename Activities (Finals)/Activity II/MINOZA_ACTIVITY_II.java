c765c52190a769ae8ba54509fcb8a2fe772f3401
MAYLENE M. MINOZA <156988633+Maylene99@users.noreply.github.com>
2024-04-15 10:43:09 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/MINOZA_ACTIVITY_II.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class MINOZA_ACTIVITY_II {
+    static void conversion(int feet){
+        int result = feet*12;
+        System.out.println(result);
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter feet: ");
+        int feet = sc.nextInt();
+        conversion(feet);
+    }
+
+}
