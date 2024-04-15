f24abc4ee2ab88c9284be4a1660bd41bb65d1eaa
Johan Carl Demotor <157077716+johancarldemotor@users.noreply.github.com>
2024-04-15 10:48:45 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/DEMOTOR_ACTIVITY_II.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class DEMOTOR_ACTIVITY_II{
+    static void feetToInches(double feet)
+       double res = * 12;
+       System.out.println(res);
+   }
+
+    public static void main(string[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print(s:"Enter the feet");
+        double feet = sc.nextDouble();
+    
+    }
+}
