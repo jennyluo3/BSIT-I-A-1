5f5e7a843446cd9c562f0289fce7ca7bd74a9a4f
John franz O. Caliguid <158005719+johnfrank7@users.noreply.github.com>
2024-04-15 10:44:14 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/CALIGUID_ACTIVITY_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CALIGUID_ACTIVITY_II{
+	static int converts(int feet){
+		int inches = feet * 12;
+		System.out.println(inches);
+		return inches;
+		}
+	public static void main(String [] args){
+	    Scanner input = new Scanner(System.in);
+		System.out.println("Enter feet: ");
+		int num = input.nextInt();
+
+		converts(num);
+		}
+	}
