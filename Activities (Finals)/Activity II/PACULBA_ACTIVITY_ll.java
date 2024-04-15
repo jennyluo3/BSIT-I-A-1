d630528c0bebd4506c682cda6fde825ee11c9a3f
Paculba,Angelyn <158006374+paculbaange@users.noreply.github.com>
2024-04-15 10:49:26 +0800

    PACULBA_ACTIVITY_ll.java

--- Activities (Finals)/Activity II/PACULBA_ACTIVITY_ll.java
+++ Activities (Finals)/Activity II/PACULBA_ACTIVITY_ll.java
@@ -1,14 +1,13 @@
-import java.util.Scanner;
+import java.util.Scanner; 
 public class PACULBA_ACTIVITY_II {
-    static void conversion(int feet){
-        int result = feet*12;
-        System.out.println(result);
+        static void inch(double feet){
+            double res= feet*12;
+            System.out.println(res);
     }
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter feet: ");
-        int feet = sc.nextInt();
-        conversion(feet);
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter number of feet:" );
+        double feet= input.nextDouble();
+        inch(feet);
     }
-
-}
+} 
