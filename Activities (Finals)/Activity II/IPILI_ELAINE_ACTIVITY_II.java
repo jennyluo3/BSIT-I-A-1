6afeb2f1bfbebcea649eca8a603aa93ff4497f01
Elaine Nicole Ipili <157080275+ipilielainenicole@users.noreply.github.com>
2024-04-15 10:45:30 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/IPILI_ELAINE_ACTIVITY_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class IPILI_ELAINE_ACTIVITY_II {
+    static void feetToInches= (double feet){
+       double res= feet*12;
+        System.out.println("res");
+            
+ }
+ public ctatic void main(String[]args){
+    Scanner sc= new Scanner(System,in);
+    System.out.print("Enter a feet:");
+    double feet= sc.nextDouble();
+    feetToInches(feet);
+    
+ }
