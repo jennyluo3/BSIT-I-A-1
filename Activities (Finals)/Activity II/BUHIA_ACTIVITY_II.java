7f1012351d6f88d70ad2a4a8ec9edf823923197e
Christian09232004 <158007121+Christian09232004@users.noreply.github.com>
2024-04-15 10:51:50 +0800

    Update BUHIA_ACTIVITY_II.java

--- Activities (Finals)/Activity II/BUHIA_ACTIVITY_II.java
+++ Activities (Finals)/Activity II/BUHIA_ACTIVITY_II.java
@@ -3,13 +3,12 @@ public class BUHIA_ACTIVITY_II{
     static void feettoinches(double feet){
         double res = feet * 12;
         System.out.println(res);
-
+    }
         public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your feet: ");
         double feet = sc.nextDouble();
-        converse(feet);
+        feettoinches(feet);
 
         }
     }
-}
