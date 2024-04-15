670d92019bad6bab7c11e7bd604f78333c4ff584
Gessyl Mae F. Briones <156914040+gessylbriones@users.noreply.github.com>
2024-04-15 10:46:04 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/BRIONES_ACTIVITY_II.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class BRIONES_ACTIVITY_II{
+  static void feetToInch(double feet){
+    double res = feet * 12;
+    System.out.println(res);
+}
+  
+  public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+  System.out.print("Enter the feet: ");
+  double feet = sc.nextDouble();
+  feetToInch(feet);
+
+}
+}
+
+  
