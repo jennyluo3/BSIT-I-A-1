26433434783ae761aae72f81ad7f4349bfcd84fd
Jenny Luo Cano Hiyan <158005724+jennyluo3@users.noreply.github.com>
2024-04-15 10:47:23 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/HIYAN_ACTIVITY_II.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class HIYAN_ACTIVITY_II {
+
+    static int getfeet(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Feet: ");
+        return sc.nextInt();
+    }
+    public static void main(String []args){
+        int feet = getfeet();
+        int inches = feet * 12;
+        System.out.println(inches);
+      
+    }
+    
+}
