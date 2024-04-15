c5b186424505c69183a3c92d4dc1a2a37b5945ca
Rhea Mae L. Jayme <157574770+rheajayme@users.noreply.github.com>
2024-04-15 10:41:48 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/JAYME_ACTIVITY_II.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class JAYME_ACTIVITY_II {
+ static void feetToInches=(double feet){
+  double res = feet * 12;
+  System.out.println(res);
+ }
+  public static void main(String[]args);
+ Scanner sc=new Scanner(System.in);
+ System.out.print("Enter the feet: ");
+ double feet = sc.nextDouble();
+ feetToInches(feet);
+}
