79063d46ac7a4aa0bcf7c724c3cfd1a96a730d4c
Gian Gadiano Villarmia <157211284+GianVillarmia18@users.noreply.github.com>
2024-04-15 10:42:29 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/VILLARMIA_ACTIVITY_II.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class VILLARMIA_ACTIVITY_II{
+        public static double feetToInches(double feet) {
+            return feet * 12;
+        }
+    
+        public static void main(String[] args) {
+            double feet = 12;
+            double inches = feetToInches(feet);
+            System.out.println(feet + " feet is equal too " + inches + " inches.");
+    }
+ }
+
