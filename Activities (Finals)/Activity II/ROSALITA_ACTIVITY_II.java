3e1b728bc6dd3c461081c1077b00523a7803c7ff
A Jay R. Rosalita <157581445+ajayrosalita5@users.noreply.github.com>
2024-04-15 10:52:36 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/ROSALITA_ACTIVITY_II.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class ROSALITA_ACTIVITY_II{
+    static void feetToInches(double feet) {
+        double res = feet * 12;
+        System.out.print(res);
+
+    }
+    public static void main (String[]args){
+        int feet = getFree();
+        int inches = feet * 12;
+        System.out.print(inches);
+    }
+
+
+}
