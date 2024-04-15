9725759520781dfe7aee932226c5d7347b4af080
Sereneo Jiethmar C <158004974+sjiethmar@users.noreply.github.com>
2024-04-15 10:46:20 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/SERENEO_ACTIVITY_II.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class SERENEO_ACTIVITY_II {
+    public static void main(String[] args) {
+        int feet = getfeet();
+        int inches = feet * 12;
+        System.out.println(inches);
+    }
+
+    static int getfeet() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Feet: ");
+        return sc.nextInt();
+    }
+}
