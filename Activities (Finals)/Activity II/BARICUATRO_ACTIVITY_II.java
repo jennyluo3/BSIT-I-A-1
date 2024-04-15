cb02cc90b0510af1cb0c08d5960f6dfb446326fa
Ethel Rose U. Baricuatro <158002930+itilrozz@users.noreply.github.com>
2024-04-15 10:40:23 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/BARICUATRO_ACTIVITY_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class BARICUATRO_ACTIVITY_II {
+    static void conversion(int feet){
+        int result = feet*12;
+        System.out.println(result);
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter feet: ");
+        int feet = sc.nextInt();
+        conversion(feet);
+    }
+
+}
+
