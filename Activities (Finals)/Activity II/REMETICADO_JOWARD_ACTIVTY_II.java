2e115a1db1f219643bc2f7143aae0ec2595f85d3
Joward Remeticado <157957512+Wardo30@users.noreply.github.com>
2024-04-15 10:41:27 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/REMETICADO_JOWARD_ACTIVTY_II.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class REMETICADO_JOWARD_ACTIVTY_II {
+    static void con(int a){
+        int res = a * 12;
+        System.out.print(res);
+}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int a = sc.nextInt();
+        con(a);
+        
+    }
+    
+}
