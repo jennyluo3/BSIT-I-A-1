f205e21ac511771d785ba60dd734209a796ce155
Sarcon Chane B <158005300+chanesarcon@users.noreply.github.com>
2024-04-14 19:46:52 -0700

    SARCON_ACTIVITY_II.java

---
+++ Activities (Finals)/Activity II/SARCON_ACTIVITY_II.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; 
+public class SARCON_ACTIVITY_II {   
+     
+    static void feetToInches(double feet) {
+        double res - feet * 12;|
+         System.out.println(res);
+        
+        public static void main(String[] args){
+         Scanner sc - new Scanner (System.in);
+         System.out print(s: "Enter the feet: "); 
+         double feet - sc.nextDouble(); 
+         feetToInches (feet);
+    }
+}
