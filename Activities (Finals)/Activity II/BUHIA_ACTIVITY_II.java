1f12311f09d5f913a819c095c607aa703bddb572
Christian09232004 <158007121+Christian09232004@users.noreply.github.com>
2024-04-15 10:48:31 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/BUHIA_ACTIVITY_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class BUHIA_ACTIVITY_II{
+    static void feettoinches(double feet){
+        double res = feet * 12;
+        System.out.println(res);
+
+        public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your feet: ");
+        double feet = sc.nextDouble();
+        converse(feet);
+
+        }
+    }
+}
