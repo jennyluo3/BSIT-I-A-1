3542430945c928e80de6685633b448ea32b3f968
Lovenel Canillo <158016531+Lovenel19@users.noreply.github.com>
2024-05-08 10:21:39 +0800

    Update CANILLO_ACTIVITY_1.java

--- Activities/Activity I/CANILLO_ACTIVITY_1.java
+++ Activities/Activity I/CANILLO_ACTIVITY_1.java
@@ -1,17 +1,24 @@
-public class CANILLO_ACTIVITY_I{
-
-    public static void main(String[]args){
-
-        int a = 10;
-        int b = 5;
-        int c = 4;
-
-        int sum = a + b + c;
-        int diff = a - b - c;
-        int prod = a * b * c;
-        int quot = a / b / c;
-
-                System.out.println("Sum: " + sum);
-                System.out.println("Difference: " + diff);
-                System.out.println("Product: " + prod);
-                System.out.println("Quotient: " + quot);
+import java.util.Scanner;
+public class CANILLO_ACTIVITY_I {
+    public static void main(String[] args) {
+        
+        double var1, var2, var3;
+        
+        
+        var1 = 10.0;
+        var2 = 5.0;
+        var3 = 2.0;
+        
+        
+        double sum = var1 + var2 + var3;
+        double difference = var1 - var2 - var3;
+        double product = var1 * var2 * var3;
+        double quotient = var1 / var2 / var3;
+        
+       
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+        System.out.println("Quotient = " + quotient);
+    }
+}
