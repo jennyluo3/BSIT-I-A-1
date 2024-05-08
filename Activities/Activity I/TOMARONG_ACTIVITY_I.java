ee1ce1f63e4a1de71d14aeb4214416c9c3a74f6c
Laiza Mae Tomarong <156989768+Laizamae17@users.noreply.github.com>
2024-05-08 10:28:21 +0800

    Update and rename TOMARONG_Activity_1.txt to TOMARONG_ACTIVITY_I.java

---
+++ Activities/Activity I/TOMARONG_ACTIVITY_I.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class TOMARONG_ACTIVITY_I {
+    public static void main(String[] args) {
+        
+        double var1, var2, var3;
+        
+        
+        var1 = 10.0;
+        var2 = 5.0;
+        var3 = 2.0;
+        
+        
+        double sum = var1 + var2 + var3;
+        double difference = var1 - var2 - var3;
+        double product = var1 * var2 * var3;
+        double quotient = var1 / var2 / var3;
+        
+       
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+        System.out.println("Quotient = " + quotient);
+    }
+}
