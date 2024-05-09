407036ea42361266e69f8cc0317ff26778a8edff
Laiza Mae Tomarong <156989768+Laizamae17@users.noreply.github.com>
2024-05-09 14:50:12 +0800

    Update and rename TOMARONG_ACTIVITY_1.java to TOMARONG_ACTIVITY_I.java

--- Activities (Midterm)/Activity I/TOMARONG_ACTIVITY_I.java
+++ Activities (Midterm)/Activity I/TOMARONG_ACTIVITY_I.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class TOMARONG_ACTIVITY_1{
+public class TOMARONG_ACTIVITY_I{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
