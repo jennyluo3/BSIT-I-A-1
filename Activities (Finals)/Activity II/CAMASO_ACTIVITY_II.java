586377e1a32fe98e0a23e939a1c60716d8189e93
John Lloyd Camaso <151697948+redwind419@users.noreply.github.com>
2024-05-09 13:32:44 +0800

    Update CAMASO_ACTIVITY_II.java

--- Activities (Finals)/Activity II/CAMASO_ACTIVITY_II.java
+++ Activities (Finals)/Activity II/CAMASO_ACTIVITY_II.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class CAMASO_ACTIVITY_II {
     static double convert(double feet){
         double inches = feet * 12.0;
-        System.out.println(inches);
+        System.out.println("Inches: : "+ inches);
         return inches;
     }
 
     public static void main(String[] args) {
-		System.out.println("Please Input A Number");
+                System.out.println("Please Input A Number In Feet");
         Scanner sc = new Scanner(System.in);
         double num = sc.nextDouble();
 
