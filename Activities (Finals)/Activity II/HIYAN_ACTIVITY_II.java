71f38f6665af076ee03e4a98519dcaf89446fbc3
Jenny Luo Cano Hiyan <158005724+jennyluo3@users.noreply.github.com>
2024-05-09 18:26:06 +0800

    HIYAN_ACTIVITY_II.java

--- Activities (Finals)/Activity II/HIYAN_ACTIVITY_II.java
+++ Activities (Finals)/Activity II/HIYAN_ACTIVITY_II.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
-public class HIYAN_ACTIVITY_II {
+public class HIYAN_ACTIVITY_II{
 
     static int getfeet(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Feet: ");
+        System.out.print("Enter Feet: ");
         return sc.nextInt();
     }
     public static void main(String []args){
         int feet = getfeet();
         int inches = feet * 12;
-        System.out.println(inches);
+        System.out.println("Inches: "+inches);
       
     }
     
