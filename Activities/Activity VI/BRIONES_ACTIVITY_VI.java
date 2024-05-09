af99b3550d8dea06be866d7d560db1b84a7a2ebe
Gessyl Mae F. Briones <156914040+gessylbriones@users.noreply.github.com>
2024-05-09 16:36:44 +0800

    BRIONES_ACTIVITY_VI.java

--- Activities/Activity VI/BRIONES_ACTIVITY_VI.java
+++ Activities/Activity VI/BRIONES_ACTIVITY_VI.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-    public class BRIONES_ACTIVITY_6{
+    public class BRIONES_ACTIVITY_VI{
         public static void main(String []args){
             Scanner sc = new Scanner(System.in);
 
