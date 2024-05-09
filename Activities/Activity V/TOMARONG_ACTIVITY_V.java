3d09201a4cd691dfe2617ddbb6db2dcec95dbedb
Laiza Mae Tomarong <156989768+Laizamae17@users.noreply.github.com>
2024-05-09 14:06:42 +0800

    Update and rename TOMARONG_ACTIVITY_5.java to TOMARONG_ACTIVITY_V.java

--- Activities/Activity V/TOMARONG_ACTIVITY_V.java
+++ Activities/Activity V/TOMARONG_ACTIVITY_V.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class TOMARONG_ACTIVITY_5{
+public class TOMARONG_ACTIVITY_V{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
     
