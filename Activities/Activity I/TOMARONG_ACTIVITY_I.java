f9a0c314dbfdb51e7b204b707baa11024711e760
Laiza Mae Tomarong <156989768+Laizamae17@users.noreply.github.com>
2024-05-09 14:36:17 +0800

    Update TOMARONG_ACTIVITY_I.java

--- Activities/Activity I/TOMARONG_ACTIVITY_I.java
+++ Activities/Activity I/TOMARONG_ACTIVITY_I.java
@@ -1,24 +1,20 @@
 import java.util.Scanner;
-public class TOMARONG_ACTIVITY_I {
-    public static void main(String[] args) {
-        
-        double var1, var2, var3;
-        
-        
-        var1 = 36.5;
-        var2 = 10.5;
-        var3 = 4.5;
-        
-        
-        double sum = var1 + var2 + var3;
-        double difference = var1 - var2 - var3;
-        double product = var1 * var2 * var3;
-        double quotient = var1 / var2 / var3;
-        
-       
-        System.out.println("Sum = " + sum);
-        System.out.println("Difference = " + difference);
-        System.out.println("Product = " + product);
-        System.out.println("Quotient = " + quotient);
-    }
+public class TOMARONG_ACTIVITY_I{
+          public static void main(String[] args) {
+           Scanner sc = new Scanner(System.in);
+           
+           int l= 10;
+           int m= 50;
+           int e= 40;
+
+           int sum= l+m+e;
+           int diff= l-m-e;
+           int prod= l*m*e;
+           int quot= l/m/e;
+
+           System.out.println("Sum:" +sum);
+           System.out.println("Difference:" +diff);
+           System.out.println("Product:" +prod);
+           System.out.println("Quotient:" +quot);
+       }
 }
