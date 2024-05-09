23ccda6866a8a653fb9611642c8bbfbac2e39d63
jaybienairy <158005907+jaybienairy@users.noreply.github.com>
2024-05-09 15:27:10 +0800

    Update and rename NAIRY_ACTIVITY_1.java to NAIRY_ACTIVITY_I.java

---
+++ Activities/Activity I/NAIRY_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+public class NAIRY_ACTIVITY_I{
+   
+    public static void main(String[]args){
+        
+        int q = 15;
+        int w = 25;
+        int e = 45;
+        
+        int sum = q + w + e;
+        int diff = q - w - e;
+        int prod = q * w * e;
+        int quot = q / w / e;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + diff);
+        System.out.println("Product: " + prod);
+        System.out.println("Quotient: " + quot);
+
+    }
+}
