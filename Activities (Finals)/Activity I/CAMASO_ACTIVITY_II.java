fce30fa4140a777ec406fe3ee8902fc51bbd0802
John Lloyd Camaso <151697948+redwind419@users.noreply.github.com>
2024-05-09 13:42:09 +0800

    Update CAMASO_ACTIVITY_I.java

--- Activities (Finals)/Activity I/CAMASO_ACTIVITY_II.java
+++ Activities (Finals)/Activity I/CAMASO_ACTIVITY_II.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
 
-public class CAMASO_ACTIVITY_II {
-    static double convert(double feet){
-        double inches = feet * 12.0;
-        System.out.println(inches);
-        return inches;
-    }
-
+public class CAMASO_ACTIVITY_I{
     public static void main(String[] args) {
-		System.out.println("Please Input A Number");
         Scanner sc = new Scanner(System.in);
-        double num = sc.nextDouble();
-
-        convert(num);
+        
+        int length = getLength(sc);
+        int width = getWidth(sc);
+        int area = length * width;
+        
+        System.out.println("Area = " + area);
+    }
+    
+    public static int getLength(Scanner sc){
+        System.out.println("Enter the length of the Rectangle: ");
+        return sc.nextInt();
+    }
+    
+    public static int getWidth(Scanner sc){
+        System.out.println("Enter the width of the Rectangle: ");
+        return sc.nextInt();
     }
 }
