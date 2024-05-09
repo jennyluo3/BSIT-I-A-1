a1b1fedf339ad5f5ca5750c5d9e6902e7071fec5
Jenny Luo Cano Hiyan <158005724+jennyluo3@users.noreply.github.com>
2024-05-09 13:19:28 +0800

    Update and rename Hiyan_Activity_1.java to HIYAN_ACTIVITY_I.java

--- Activities (Midterm)/Activity I/HIYAN_ACTIVITY_I.java
+++ Activities (Midterm)/Activity I/HIYAN_ACTIVITY_I.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Hiyan_Activity_1 {
+public class HIYAN_ACTIVITY_I {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
 
