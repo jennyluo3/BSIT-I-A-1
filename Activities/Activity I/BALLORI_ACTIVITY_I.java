55865ff28027e514b3aba3f194bd9b17164aed5b
Van Cyril Ballori <158006267+VanCyrilBallori@users.noreply.github.com>
2024-05-09 13:15:37 +0800

    Update BALLORI_ACTIVITY_I.java

--- Activities/Activity I/BALLORI_ACTIVITY_I.java
+++ Activities/Activity I/BALLORI_ACTIVITY_I.java
@@ -1,21 +1,18 @@
 public class BALLORI_ACTIVITY_I{
     public static void main(String []args){
 
-        int a = 10, b = 10, c = 5;
-        int sum = a + b + c;
-        int diff = a - b - c;
-        int prod = a * b * c;
-        int quot = a / b / c;
+        double Arr1 = 0.5;
+        double Arr2 = 0.5;
+        double sum, diff, prod, quot;
+
+        sum = Arr1 + Arr2;
+        diff = Arr1 - Arr2;
+        prod = Arr1 * Arr2;
+        quot = Arr1 / Arr2;
 
         System.out.println("Sum: "+sum);
         System.out.println("Difference: "+diff);
         System.out.println("Product: "+prod);
         System.out.println("Quotient: "+quot);
-        
-        
-
-
     }
 }
-
-    
